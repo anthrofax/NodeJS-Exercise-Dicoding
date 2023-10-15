a558852d331be67785e7bd872a4581e22df2e68f
anthrofax <afridhoikhsan@gmail.com>
2023-10-15 11:21:15 +0700

    first init

---
+++ 04-events/index.js
@@ -0,0 +1,12 @@
+// TODO 1
+const {EventEmitter} = require('events')
+ 
+const birthdayEventListener = (name) => {
+    console.log(`Happy birthday ${name}!`);
+  }
+   
+  // TODO 2
+   
+  // TODO 3
+   
+  // TODO 4
---
+++ app.js
@@ -0,0 +1,6 @@
+let var1 = process.argv[0];
+let var2 = process.argv[1];
+let var3 = process.argv[2];
+let var4 = process.argv[3];
+
+console.log(`${var1}, ${var2}, ${var3}, ${var4}`);
