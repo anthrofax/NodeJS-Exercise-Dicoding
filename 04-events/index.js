e642891a1d41795757b7b92712ffd011fc14a553
anthrofax <afridhoikhsan@gmail.com>
2023-10-15 11:27:38 +0700

    latihan 4 - events selesai

--- 04-events/index.js
+++ 04-events/index.js
@@ -1,12 +1,15 @@
 // TODO 1
-const {EventEmitter} = require('events')
- 
+const { EventEmitter } = require("events");
+
 const birthdayEventListener = (name) => {
-    console.log(`Happy birthday ${name}!`);
-  }
-   
-  // TODO 2
-   
-  // TODO 3
-   
-  // TODO 4
+  console.log(`Happy birthday ${name}!`);
+};
+
+// TODO 2
+const myEmitter = new EventEmitter();
+
+// TODO 3
+myEmitter.on("birthday", birthdayEventListener);
+
+// TODO 4
+myEmitter.emit("birthday", "Afridho Ikhsan")
